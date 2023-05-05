2e40ebcc6270b2511f54fdeaf98e841c39c15132
Zerumi <46845708+Zerumi@users.noreply.github.com>
2023-05-06 01:46:41 +0300

    fix: test class name
    Signed-off-by: Zerumi <46845708+Zerumi@users.noreply.github.com>

---
+++ server/src/test/java/LoginAndTryToSendCommandTest.java
@@ -0,0 +1,27 @@
+import authorization.authCredentials.AuthenticationData;
+import clientLogic.AuthorizeManager;
+import commandLogic.CommandDescription;
+import commandLogic.commandReceiverLogic.callers.ExternalBaseReceiverCaller;
+import exceptions.authorizationExceptions.AuthorizeException;
+import org.junit.jupiter.api.Test;
+import requestLogic.CallerBack;
+import requestLogic.requestWorkers.RequestWorkerManager;
+import requestLogic.requests.ServerRequest;
+import requests.CommandClientRequest;
+import serverLogic.DatagramServerConnectionFactory;
+
+import java.net.InetSocketAddress;
+
+public class LoginAndTryToSendCommandTest {
+    @Test
+    public void test() throws AuthorizeException {
+        var b = AuthorizeManager.authorize(new CallerBack(new InetSocketAddress(Main.PORT).getAddress(), Main.PORT),
+                new AuthenticationData("zerumi", new char[]{'q', 'w', 'e', 'r', 't', 'y'}));
+        System.out.println(b);
+
+        new RequestWorkerManager().workWithRequest(new ServerRequest(new CommandClientRequest(
+                new CommandDescription("info", new ExternalBaseReceiverCaller()), new String[]{"info"}),
+                new CallerBack(new InetSocketAddress(Main.PORT).getAddress(), Main.PORT),
+                new DatagramServerConnectionFactory().initializeServer(Main.PORT)));
+    }
+}
